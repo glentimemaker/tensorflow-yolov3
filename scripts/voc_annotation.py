8129f3ab72677faa3010807cdc7168b0374be42c
Ubuntu <ubuntu@chenlei-workstation.us-west1-b.c.procedure-monitoring-279403.internal>
2021-04-06 08:35:30 +0000

    fix

--- scripts/voc_annotation.py
+++ scripts/voc_annotation.py
@@ -47,9 +47,9 @@ def convert_voc_annotation(data_path, data_type, anno_path, class_num, use_diffi
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_path", default="/home/chenlei/thesis/tensorflow-yolov3/data/VOC/")
-    parser.add_argument("--train_annotation", default="/home/chenlei/thesis/tensorflow-yolov3/data/dataset/voc_train.txt")
-    parser.add_argument("--test_annotation",  default="/home/chenlei/thesis/tensorflow-yolov3/data/dataset/voc_test.txt")
+    parser.add_argument("--data_path", default="data/VOC/")
+    parser.add_argument("--train_annotation", default="data/dataset/voc_train.txt")
+    parser.add_argument("--test_annotation",  default="data/dataset/voc_test.txt")
     flags = parser.parse_args()
 
     if os.path.exists(flags.train_annotation):os.remove(flags.train_annotation)
@@ -61,8 +61,8 @@ if __name__ == '__main__':
     test_class_num = {}
     num3, test_class_num = convert_voc_annotation(os.path.join(flags.data_path, 'test/VOCdevkit/VOC2007'),  'test', flags.test_annotation, test_class_num, False)
     print('=> The number of image for train is: %d\tThe number of image for test is:%d' %(num1+num2, num3))
-    json.dump(class_num, open('/home/chenlei/thesis/tensorflow-yolov3/data/dataset/class_num_train.json', 'w'))
-    json.dump(test_class_num, open('/home/chenlei/thesis/tensorflow-yolov3/data/dataset/class_num_test.json', 'w'))
+    json.dump(class_num, open('data/dataset/class_num_train.json', 'w'))
+    json.dump(test_class_num, open('data/dataset/class_num_test.json', 'w'))
 
 
 
